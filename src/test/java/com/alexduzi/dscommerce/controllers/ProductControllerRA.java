97024a25a24990740d08f92bfb3a11b45ded7a54
alexduzi <duzihd@gmail.com>
2025-05-20 14:47:18 -0300

    ProductControllerRA findAll integration tests

--- src/test/java/com/alexduzi/dscommerce/controllers/ProductControllerRA.java
+++ src/test/java/com/alexduzi/dscommerce/controllers/ProductControllerRA.java
@@ -1,19 +1,22 @@
 package com.alexduzi.dscommerce.controllers;
 
-import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
-import static org.hamcrest.Matchers.*;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 public class ProductControllerRA {
 
     private Long existingProductId, nonExistingProductId;
+    private String productName;
 
     @BeforeEach
     public void setUp() {
         baseURI = "http://localhost:8080";
+
+        productName = "Macbook";
     }
 
     @Test
@@ -30,5 +33,34 @@ public class ProductControllerRA {
                 .body("price", is(2190.0F))
                 .body("categories.id", hasItems(2, 3))
                 .body("categories.name", hasItems("Eletrônicos", "Computadores"));
+    }
+
+    @Test
+    public void findAllShouldReturnPageProductsWhenProductNameIsEmpty() {
+        given()
+                .get("/products?page=0")
+                .then()
+                .statusCode(200)
+                .body("content.name", hasItems("Macbook Pro", "PC Gamer Tera"));
+    }
+
+    @Test
+    public void findAllShouldReturnPageProductsWhenProductNameIsNotEmpty() {
+        given()
+                .get("/products?name={productName}", productName)
+                .then()
+                .statusCode(200)
+                .body("content.id[0]", is(3))
+                .body("content.name[0]", equalTo("Macbook Pro"))
+                .body("content.price[0]", is(1250.0F));
+    }
+
+    @Test
+    public void findAllShouldReturnPagedProductsWithPriceGreaterThan2000() {
+        given()
+                .get("/products?page=0&size=25")
+                .then()
+                .statusCode(200)
+                .body("content.findAll { it.price > 2000 }.name", hasItems("Smart TV", "PC Gamer Weed"));
     }
 }
